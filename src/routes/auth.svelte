e71fec95bc712e042515c9c068a686fc0f0a952b
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-02-16 21:42:18 +0000

    auth pages added

---
+++ src/routes/auth.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import SignIn from '../lib/signIn.svelte';
+	import SignUp from '../lib/signUp.svelte';
+</script>
+
+<div class="grid place-items-center w-full h-screen">
+	<SignIn />
+	<SignUp />
+</div>
